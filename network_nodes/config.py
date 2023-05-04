a05efc4cd972f5d6e7fc5e9b18492f93b3bd0ffa
Seba-Tayser <118387035+Seba-Tayser@users.noreply.github.com>
2023-05-04 19:06:40 -0400

    Add files via upload

--- network_nodes/config.py
+++ network_nodes/config.py
@@ -1,5 +1,4 @@
-iface_name = 'enp6s0f0'
-filter_string = 'tcp || udp && ip && tcp port not 443 && udp port not 123'
-destination = 'seba@node0:/users/seba/node0/logs'
-logs_path = '/users/seba/node0/logs'
+filter_string = 'tcp || udp && tcp port not 443 && udp port not 123'
 stop_port = 13897
+capture_filter_filename = f'capture_filter'
+
---
+++ network_nodes/pre_capture.py
@@ -0,0 +1,51 @@
+import subprocess
+import config
+import re
+
+
+
+filter_string = config.filter_string
+capture_filter_filename = config.capture_filter_filename
+tcp_ports = set()
+udp_ports = set()
+
+
+def get_tcp_open_ports():
+ process = subprocess.run(['netstat', '-ant'], capture_output=True)
+ report = process.stdout.decode().splitlines()
+ for i in report[2:]:
+       tcp_ports.add(i.split()[3].split(":")[-1])
+ return tcp_ports
+
+
+def get_udp_open_ports():
+ process = subprocess.run(['netstat', '-anu'], capture_output=True)
+ report = process.stdout.decode().splitlines()
+ for i in report[2:]:
+        udp_ports.add(i.split()[3].split(":")[-1])       
+ return udp_ports
+
+
+def generate_capturing_filter():
+  global filter_string      
+  for port in udp_ports:
+        if(port == "*"):
+              continue 
+        filter_string += ' && udp port not ' + str(port) + ' '
+
+  for port in tcp_ports:
+         if(port == "*"):
+                 continue 
+         filter_string += '&& tcp port not ' + str(port) + ' '
+  return filter_string
+
+def write_filter ():
+     with open( capture_filter_filename , 'w') as out_file:
+          out_file.write("%s\n" % filter_string)
+
+if __name__ == '__main__':
+     get_tcp_open_ports()
+     get_udp_open_ports()
+     generate_capturing_filter()
+     write_filter()
+     print("Done")
